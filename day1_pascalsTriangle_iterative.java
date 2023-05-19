6477e34818630cb74427913a8d7ed52834dd1312
santosh <114974242+santosh-telusko@users.noreply.github.com>
2023-05-19 23:23:29 +0530

    Update day1_pascalsTriangle_iterative.java

--- day1_pascalsTriangle_iterative.java
+++ day1_pascalsTriangle_iterative.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PascalTriangle {
+public class Main {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.print("No of rows: ");
